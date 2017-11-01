9b98092f921d9e5cdca9bfe18ac2fabcafdc069a
chapeaua <chapeaua@tcd.ie>
2017-11-01 20:15:24 +0000

    Made my new Node class for the implementation

---
+++ DAGLCA/src/Node.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class Node{
+			int data;
+			char ident;
+			List<Node> parents = new ArrayList<Node>();
+			List<Node> children = new ArrayList<Node>();
+		}
+
