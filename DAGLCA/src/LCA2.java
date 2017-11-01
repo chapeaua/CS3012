0f04e776937e1607a7af0b17f34b9956ef427c5a
chapeaua <chapeaua@tcd.ie>
2017-11-01 20:27:46 +0000

    Add 2 simple function an add node and a search node.Initial test work for these functions

--- DAGLCA/src/LCA2.java
+++ DAGLCA/src/LCA2.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class LCA2 {
 		public Node root;
 		
-		//creating the root of the graph
+		//creating the root of the 
 		public void makeroot(int data,char ident){
 			Node root=new Node();
 			root.data=data;
@@ -12,4 +12,38 @@ public class LCA2 {
 			root.children = new ArrayList<Node>();
 			this.root=root;
 		}
+		//Search method to find a set node using his identity
+		public Node SearchNode(char ident){
+			Node result=new Node() ;
+			if(ident==root.ident){return root;}
+			else{
+				for(int i=0;i<root.children.size();i++){
+					result=SearchNode(ident,root.children.get(i));
+				}
+				if(result!=null){return result;}			
+			}
+			return null;
+		}
+		private Node SearchNode(char ident,Node n1){
+			Node result;
+			if(ident==n1.ident){return n1;}
+			else{
+				for(int i=0;i < n1.children.size();i++){
+					result=SearchNode(ident,n1.children.get(i));
+					if(result !=null){return result;}	
+				}
+			}
+			return null;
+		}
+	//adding a set node with a identity char ,data ,and his parent	
+		public void addNode(char ident, int data,Node parent){
+			Node Node = new Node();
+			Node.ident = ident;
+			Node.children = new ArrayList<Node>();
+			Node.data = data;
+			Node.parents.add(parent);
+			parent.children.add(Node);
+		}
+		
 
+}
